c67e6b89a6a89e7e887f9bd931bf3bec36b2f42e
FabricioHA <fabricio.277672gmail.com>
2024-11-04 09:38:15 -0300

    .

--- cadastro_produto.py
+++ cadastro_produto.py
@@ -7,6 +7,7 @@
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
+
 import sys
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
@@ -24,9 +25,9 @@ class Ui_RegProductPage(object):
     def setupUi(self, RegProductPage):
         if not RegProductPage.objectName():
             RegProductPage.setObjectName(u"RegProductPage")
-        RegProductPage.resize(600, 530)
-        RegProductPage.setMinimumSize(QSize(600, 530))
-        RegProductPage.setMaximumSize(QSize(600, 530))
+        RegProductPage.resize(1100, 600)
+        RegProductPage.setMinimumSize(QSize(1100, 600))
+        RegProductPage.setMaximumSize(QSize(1100, 600))
         RegProductPage.setStyleSheet(u"background-color: #fff;\n"
 "\n"
 "\n"
@@ -35,13 +36,13 @@ class Ui_RegProductPage(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.embrulho = QGraphicsView(self.centralwidget)
         self.embrulho.setObjectName(u"embrulho")
-        self.embrulho.setGeometry(QRect(60, 70, 486, 429))
+        self.embrulho.setGeometry(QRect(65, 70, 981, 501))
         self.embrulho.setStyleSheet(u"border: 5px solid rgb(93,215,241);\n"
 "\n"
 "border-radius:10px;")
         self.textBrowser = QTextBrowser(self.centralwidget)
         self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(60, 70, 207, 27))
+        self.textBrowser.setGeometry(QRect(70, 70, 207, 27))
         self.textBrowser.setStyleSheet(u"background-color:#5DD7F1;\n"
 "\n"
 "border: nome;\n"
@@ -59,15 +60,15 @@ class Ui_RegProductPage(object):
         self.codigo.setFont(font)
         self.unidade = QLabel(self.centralwidget)
         self.unidade.setObjectName(u"unidade")
-        self.unidade.setGeometry(QRect(80, 180, 61, 16))
+        self.unidade.setGeometry(QRect(80, 200, 61, 16))
         self.unidade.setFont(font)
         self.ncm_nbm = QLabel(self.centralwidget)
         self.ncm_nbm.setObjectName(u"ncm_nbm")
-        self.ncm_nbm.setGeometry(QRect(210, 130, 61, 16))
+        self.ncm_nbm.setGeometry(QRect(330, 130, 61, 16))
         self.ncm_nbm.setFont(font)
         self.nome_produto = QLabel(self.centralwidget)
         self.nome_produto.setObjectName(u"nome_produto")
-        self.nome_produto.setGeometry(QRect(350, 130, 131, 16))
+        self.nome_produto.setGeometry(QRect(590, 130, 131, 16))
         font1 = QFont()
         font1.setFamilies([u"MS Shell Dlg 2"])
         font1.setPointSize(8)
@@ -77,56 +78,117 @@ class Ui_RegProductPage(object):
         self.nome_produto.setStyleSheet(u"")
         self.comprimento = QLabel(self.centralwidget)
         self.comprimento.setObjectName(u"comprimento")
-        self.comprimento.setGeometry(QRect(80, 240, 91, 16))
+        self.comprimento.setGeometry(QRect(80, 260, 91, 16))
         self.comprimento.setFont(font)
         self.largura = QLabel(self.centralwidget)
         self.largura.setObjectName(u"largura")
-        self.largura.setGeometry(QRect(390, 240, 61, 16))
+        self.largura.setGeometry(QRect(590, 260, 61, 16))
         self.largura.setFont(font)
         self.altura = QLabel(self.centralwidget)
         self.altura.setObjectName(u"altura")
-        self.altura.setGeometry(QRect(230, 240, 47, 13))
+        self.altura.setGeometry(QRect(330, 260, 47, 13))
         self.altura.setFont(font)
         self.fornecedor = QLabel(self.centralwidget)
         self.fornecedor.setObjectName(u"fornecedor")
-        self.fornecedor.setGeometry(QRect(80, 300, 81, 16))
+        self.fornecedor.setGeometry(QRect(80, 320, 81, 16))
         self.fornecedor.setFont(font)
         self.preso_bruto = QLabel(self.centralwidget)
         self.preso_bruto.setObjectName(u"preso_bruto")
-        self.preso_bruto.setGeometry(QRect(420, 180, 81, 16))
+        self.preso_bruto.setGeometry(QRect(810, 200, 81, 16))
         self.preso_bruto.setFont(font)
         self.peso_liquido = QLabel(self.centralwidget)
         self.peso_liquido.setObjectName(u"peso_liquido")
-        self.peso_liquido.setGeometry(QRect(300, 180, 81, 16))
+        self.peso_liquido.setGeometry(QRect(590, 200, 81, 16))
         self.peso_liquido.setFont(font)
         self.Especie = QLabel(self.centralwidget)
         self.Especie.setObjectName(u"Especie")
-        self.Especie.setGeometry(QRect(170, 180, 47, 13))
+        self.Especie.setGeometry(QRect(330, 200, 47, 13))
         self.Especie.setFont(font)
         self.preco_de_custo = QLabel(self.centralwidget)
         self.preco_de_custo.setObjectName(u"preco_de_custo")
-        self.preco_de_custo.setGeometry(QRect(230, 300, 101, 16))
+        self.preco_de_custo.setGeometry(QRect(590, 320, 101, 16))
         self.preco_de_custo.setFont(font)
         self.btn_cadastro = QPushButton(self.centralwidget)
         self.btn_cadastro.setObjectName(u"btn_cadastro")
-        self.btn_cadastro.setGeometry(QRect(230, 410, 121, 31))
+        self.btn_cadastro.setGeometry(QRect(480, 430, 181, 61))
         self.btn_cadastro.setCursor(QCursor(Qt.PointingHandCursor))
         self.btn_cadastro.setStyleSheet(u"QPushButton\n"
 "{\n"
 "    background-color: rgb(93, 215, 241); \n"
 "    border: 1px solid rgb(93, 215, 241);\n"
 "    border-radius: 5px;\n"
-"    font-size: 10px;    \n"
+"    font-size: 10px;\n"
+"	color: #fff;\n"
 "}\n"
 "QPushButton:hover\n"
 "{\n"
 "    background-color: #fff;\n"
 "    color : rgb(93, 215, 241); ;\n"
 "}")
+        self.prod_cod = QLineEdit(self.centralwidget)
+        self.prod_cod.setObjectName(u"prod_cod")
+        self.prod_cod.setGeometry(QRect(80, 150, 221, 25))
+        self.prod_nbmncm = QLineEdit(self.centralwidget)
+        self.prod_nbmncm.setObjectName(u"prod_nbmncm")
+        self.prod_nbmncm.setGeometry(QRect(330, 150, 231, 25))
+        self.prod_descricao = QLineEdit(self.centralwidget)
+        self.prod_descricao.setObjectName(u"prod_descricao")
+        self.prod_descricao.setGeometry(QRect(590, 150, 441, 25))
+        self.prod_unidade = QLineEdit(self.centralwidget)
+        self.prod_unidade.setObjectName(u"prod_unidade")
+        self.prod_unidade.setGeometry(QRect(80, 220, 221, 25))
+        self.prod_especie = QLineEdit(self.centralwidget)
+        self.prod_especie.setObjectName(u"prod_especie")
+        self.prod_especie.setGeometry(QRect(330, 220, 231, 25))
+        self.prod_pesoL = QLineEdit(self.centralwidget)
+        self.prod_pesoL.setObjectName(u"prod_pesoL")
+        self.prod_pesoL.setGeometry(QRect(590, 220, 191, 25))
+        self.prod_pesoB = QLineEdit(self.centralwidget)
+        self.prod_pesoB.setObjectName(u"prod_pesoB")
+        self.prod_pesoB.setGeometry(QRect(810, 220, 221, 25))
+        self.prod_comprimento = QLineEdit(self.centralwidget)
+        self.prod_comprimento.setObjectName(u"prod_comprimento")
+        self.prod_comprimento.setGeometry(QRect(80, 280, 221, 25))
+        self.prod_altura = QLineEdit(self.centralwidget)
+        self.prod_altura.setObjectName(u"prod_altura")
+        self.prod_altura.setGeometry(QRect(330, 280, 231, 25))
+        self.prod_largura = QLineEdit(self.centralwidget)
+        self.prod_largura.setObjectName(u"prod_largura")
+        self.prod_largura.setGeometry(QRect(590, 280, 441, 25))
+        self.prod_precoCusto = QLineEdit(self.centralwidget)
+        self.prod_precoCusto.setObjectName(u"prod_precoCusto")
+        self.prod_precoCusto.setGeometry(QRect(590, 340, 441, 25))
+        self.prod_fornecedor = QLineEdit(self.centralwidget)
+        self.prod_fornecedor.setObjectName(u"prod_fornecedor")
+        self.prod_fornecedor.setGeometry(QRect(80, 340, 481, 25))
         self.wrapperHeader = QGraphicsView(self.centralwidget)
         self.wrapperHeader.setObjectName(u"wrapperHeader")
-        self.wrapperHeader.setGeometry(QRect(0, 0, 601, 50))
+        self.wrapperHeader.setGeometry(QRect(0, 0, 1101, 50))
         self.wrapperHeader.setStyleSheet(u"background-color: rgb(0, 0, 0);")
+        self.exitButton = QPushButton(self.centralwidget)
+        self.exitButton.setObjectName(u"exitButton")
+        self.exitButton.setGeometry(QRect(980, 10, 105, 31))
+        self.exitButton.setCursor(QCursor(Qt.PointingHandCursor))
+        self.exitButton.setStyleSheet(u"QPushButton\n"
+"{\n"
+"	background-color: rgb(93, 215, 241); \n"
+"	border: 1px solid rgb(93, 215, 241);\n"
+"	border-radius: 5px;\n"
+"	font-size: 10px;\n"
+"	color: #fff;\n"
+"}\n"
+"QPushButton:hover\n"
+"{\n"
+"	background-color: #000000;\n"
+"	color : rgb(93, 215, 241);\n"
+"}\n"
+"")
+        self.homeTitle = QLabel(self.centralwidget)
+        self.homeTitle.setObjectName(u"homeTitle")
+        self.homeTitle.setGeometry(QRect(520, 20, 121, 16))
+        self.homeTitle.setStyleSheet(u"background-color: transparent;\n"
+"color: #fff;\n"
+"font-size: 12px")
         self.homeButton = QPushButton(self.centralwidget)
         self.homeButton.setObjectName(u"homeButton")
         self.homeButton.setGeometry(QRect(10, 10, 105, 31))
@@ -138,7 +200,8 @@ class Ui_RegProductPage(object):
 "	background-color: rgb(93, 215, 241); \n"
 "	border: 1px solid rgb(93, 215, 241);\n"
 "	border-radius: 5px;\n"
-"	font-size: 10px;	\n"
+"	font-size: 10px;\n"
+"	color: #fff;\n"
 "}\n"
 "QPushButton:hover\n"
 "{\n"
@@ -146,85 +209,6 @@ class Ui_RegProductPage(object):
 "	color : rgb(93, 215, 241);\n"
 "}\n"
 "")
-        self.homeTitle = QLabel(self.centralwidget)
-        self.homeTitle.setObjectName(u"homeTitle")
-        self.homeTitle.setGeometry(QRect(230, 10, 141, 31))
-        self.homeTitle.setStyleSheet(u"background-color: transparent;\n"
-"color: #fff;\n"
-"font-size: 15px")
-        self.exitButton = QPushButton(self.centralwidget)
-        self.exitButton.setObjectName(u"exitButton")
-        self.exitButton.setGeometry(QRect(490, 10, 105, 31))
-        self.exitButton.setCursor(QCursor(Qt.PointingHandCursor))
-        self.exitButton.setStyleSheet(u"QPushButton\n"
-"{\n"
-"	background-color: rgb(93, 215, 241); \n"
-"	border: 1px solid rgb(93, 215, 241);\n"
-"	border-radius: 5px;\n"
-"	font-size: 10px;	\n"
-"}\n"
-"QPushButton:hover\n"
-"{\n"
-"	background-color: #000000;\n"
-"	color : rgb(93, 215, 241);\n"
-"}\n"
-"")
-        self.prod_cod = QLineEdit(self.centralwidget)
-        self.prod_cod.setObjectName(u"prod_cod")
-        self.prod_cod.setGeometry(QRect(80, 150, 100, 25))
-        self.prod_cod.setMaxLength(4)
-        self.prod_nbmncm = QLineEdit(self.centralwidget)
-        self.prod_nbmncm.setObjectName(u"prod_nbmncm")
-        self.prod_nbmncm.setGeometry(QRect(210, 150, 100, 25))
-        self.prod_nbmncm.setMaxLength(8)
-        self.prod_descricao = QLineEdit(self.centralwidget)
-        self.prod_descricao.setObjectName(u"prod_descricao")
-        self.prod_descricao.setGeometry(QRect(350, 150, 171, 25))
-        self.prod_descricao.setMaxLength(80)
-        self.prod_unidade = QLineEdit(self.centralwidget)
-        self.prod_unidade.setObjectName(u"prod_unidade")
-        self.prod_unidade.setGeometry(QRect(80, 200, 51, 25))
-        self.prod_unidade.setMaxLength(4)
-        self.prod_pesoL = QLineEdit(self.centralwidget)
-        self.prod_pesoL.setObjectName(u"prod_pesoL")
-        self.prod_pesoL.setGeometry(QRect(300, 200, 100, 25))
-        self.prod_pesoL.setMaxLength(10)
-        self.prod_pesoL.setText("0")
-        self.prod_pesoB = QLineEdit(self.centralwidget)
-        self.prod_pesoB.setObjectName(u"prod_pesoB")
-        self.prod_pesoB.setGeometry(QRect(420, 200, 100, 25))
-        self.prod_pesoB.setMaxLength(10)
-        self.prod_pesoB.setText("0")
-        self.prod_comprimento = QLineEdit(self.centralwidget)
-        self.prod_comprimento.setObjectName(u"prod_comprimento")
-        self.prod_comprimento.setGeometry(QRect(80, 260, 100, 25))
-        self.prod_comprimento.setMaxLength(10)
-        self.prod_comprimento.setText("0")
-        self.prod_altura = QLineEdit(self.centralwidget)
-        self.prod_altura.setObjectName(u"prod_altura")
-        self.prod_altura.setGeometry(QRect(230, 260, 100, 25))
-        self.prod_altura.setMaxLength(5)
-        self.prod_altura.setText("0")
-        self.prod_largura = QLineEdit(self.centralwidget)
-        self.prod_largura.setObjectName(u"prod_largura")
-        self.prod_largura.setGeometry(QRect(390, 260, 100, 25))
-        self.prod_largura.setMaxLength(5)
-        self.prod_largura.setText("0")
-        self.prod_precoCusto = QLineEdit(self.centralwidget)
-        self.prod_precoCusto.setObjectName(u"prod_precoCusto")
-        self.prod_precoCusto.setGeometry(QRect(230, 320, 100, 25))
-        self.prod_precoCusto.setMaxLength(10)
-        self.prod_fornecedor = QLineEdit(self.centralwidget)
-        self.prod_fornecedor.setObjectName(u"prod_fornecedor")
-        self.prod_fornecedor.setGeometry(QRect(80, 320, 100, 25))
-        self.prod_fornecedor.setMaxLength(20)
-        self.prod_especie = QComboBox(self.centralwidget)
-        self.prod_especie.addItem("PRODUTO")
-        self.prod_especie.addItem("SERVIÇO")
-        self.prod_especie.setObjectName(u"prod_especie")
-        self.prod_especie.setGeometry(QRect(170, 200, 100, 25))
-        self.prod_especie.setAutoFillBackground(False)
-        self.prod_especie.setStyleSheet(u"border: 1px solid #000")
         RegProductPage.setCentralWidget(self.centralwidget)
         QWidget.setTabOrder(self.homeButton, self.exitButton)
         QWidget.setTabOrder(self.exitButton, self.prod_cod)
@@ -253,11 +237,12 @@ class Ui_RegProductPage(object):
             self.prod_nbmncm.setText("")
             self.prod_descricao.setText("")
             self.prod_unidade.setText("")
-            self.prod_pesoL.setText("0")
-            self.prod_pesoB.setText("0")
-            self.prod_comprimento.setText("0")
-            self.prod_altura.setText("0")
-            self.prod_largura.setText("0")
+            self.prod_especie.setText("")
+            self.prod_pesoL.setText("")
+            self.prod_pesoB.setText("")
+            self.prod_comprimento.setText("")
+            self.prod_altura.setText("")
+            self.prod_largura.setText("")
             self.prod_fornecedor.setText("")
             self.prod_precoCusto.setText("")
 
@@ -276,7 +261,7 @@ class Ui_RegProductPage(object):
             ncmNbmWrited = self.prod_nbmncm.text()
             descWrited = self.prod_descricao.text()
             unidWrited = self.prod_unidade.text()
-            especieWrited = self.prod_especie.currentText()
+            especieWrited = self.prod_especie.text()
             pesoLiqWrited = self.prod_pesoL.text()
             pesoBruWrited = self.prod_pesoB.text()
             comprimentoWrited = self.prod_comprimento.text()
@@ -284,7 +269,7 @@ class Ui_RegProductPage(object):
             larguraWrited = self.prod_largura.text()
             fornecedorWrited = self.prod_fornecedor.text()
             precoCustoWrited = self.prod_precoCusto.text()
-            #Fim declarando valores de entrada
+        #Fim declarando valores de entrada
 
             if codWrited != None or ncmNbmWrited != None or descWrited != None or unidWrited != None or especieWrited != None or pesoLiqWrited != None or pesoBruWrited != None or alturaWrited != None or larguraWrited != None or comprimentoWrited != None or  fornecedorWrited != None or precoCustoWrited != None:
                 cursor = banco.cursor()
@@ -328,11 +313,9 @@ class Ui_RegProductPage(object):
         self.Especie.setText(QCoreApplication.translate("RegProductPage", u"ESP\u00c9CIE", None))
         self.preco_de_custo.setText(QCoreApplication.translate("RegProductPage", u"PRE\u00c7O DE CUSTO", None))
         self.btn_cadastro.setText(QCoreApplication.translate("RegProductPage", u"CADASTRAR", None))
-        self.homeButton.setText(QCoreApplication.translate("RegProductPage", u"HOME", None))
-        self.homeTitle.setText(QCoreApplication.translate("RegProductPage", u"RESOLUTION SPEED", None))
         self.exitButton.setText(QCoreApplication.translate("RegProductPage", u"SAIR", None))
-        self.prod_especie.setItemText(0, QCoreApplication.translate("RegProductPage", u"PRODUTO", None))
-        self.prod_especie.setItemText(1, QCoreApplication.translate("RegProductPage", u"SERVI\u00c7O", None))
+        self.homeTitle.setText(QCoreApplication.translate("RegProductPage", u"Cadastro de Produto", None))
+        self.homeButton.setText(QCoreApplication.translate("RegProductPage", u"HOME", None))
     # retranslateUi
 
 if __name__ == "__main__":
--- pedido_venda.py
+++ pedido_venda.py
@@ -7,6 +7,7 @@
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
+
 import sys
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
@@ -24,15 +25,15 @@ class Ui_PedVendaPage(object):
     def setupUi(self, PedVendaPage):
         if not PedVendaPage.objectName():
             PedVendaPage.setObjectName(u"PedVendaPage")
-        PedVendaPage.resize(600, 530)
-        PedVendaPage.setMinimumSize(QSize(600, 530))
-        PedVendaPage.setMaximumSize(QSize(600, 530))
+        PedVendaPage.resize(1100, 600)
+        PedVendaPage.setMinimumSize(QSize(1100, 600))
+        PedVendaPage.setMaximumSize(QSize(1100, 600))
         PedVendaPage.setStyleSheet(u"background-color: #fff;")
         self.centralwidget = QWidget(PedVendaPage)
         self.centralwidget.setObjectName(u"centralwidget")
         self.embrulho_2 = QGraphicsView(self.centralwidget)
         self.embrulho_2.setObjectName(u"embrulho_2")
-        self.embrulho_2.setGeometry(QRect(60, 80, 486, 429))
+        self.embrulho_2.setGeometry(QRect(60, 80, 981, 491))
         self.embrulho_2.setStyleSheet(u"border: 5px solid rgb(93,215,241);\n"
 "\n"
 "border-radius:10px;")
@@ -50,58 +51,59 @@ class Ui_PedVendaPage(object):
 "border-top-left-radius: 10px;")
         self.line_npedido = QLineEdit(self.centralwidget)
         self.line_npedido.setObjectName(u"line_npedido")
-        self.line_npedido.setGeometry(QRect(80, 170, 91, 25))
+        self.line_npedido.setGeometry(QRect(80, 190, 291, 25))
         self.line_tipovenda = QLineEdit(self.centralwidget)
         self.line_tipovenda.setObjectName(u"line_tipovenda")
-        self.line_tipovenda.setGeometry(QRect(410, 170, 91, 25))
+        self.line_tipovenda.setGeometry(QRect(700, 190, 291, 25))
         self.line_ordemcompra = QLineEdit(self.centralwidget)
         self.line_ordemcompra.setObjectName(u"line_ordemcompra")
-        self.line_ordemcompra.setGeometry(QRect(410, 290, 111, 25))
+        self.line_ordemcompra.setGeometry(QRect(700, 260, 291, 25))
         self.line_cliente = QLineEdit(self.centralwidget)
         self.line_cliente.setObjectName(u"line_cliente")
-        self.line_cliente.setGeometry(QRect(240, 170, 91, 25))
+        self.line_cliente.setGeometry(QRect(390, 190, 291, 25))
         self.line_prazopgt = QLineEdit(self.centralwidget)
         self.line_prazopgt.setObjectName(u"line_prazopgt")
-        self.line_prazopgt.setGeometry(QRect(80, 290, 91, 25))
+        self.line_prazopgt.setGeometry(QRect(80, 260, 281, 25))
         self.line_vendedor = QLineEdit(self.centralwidget)
         self.line_vendedor.setObjectName(u"line_vendedor")
-        self.line_vendedor.setGeometry(QRect(240, 290, 91, 25))
+        self.line_vendedor.setGeometry(QRect(390, 260, 291, 25))
         self.lb_npedido = QLabel(self.centralwidget)
         self.lb_npedido.setObjectName(u"lb_npedido")
-        self.lb_npedido.setGeometry(QRect(80, 150, 81, 16))
+        self.lb_npedido.setGeometry(QRect(80, 170, 81, 16))
         font = QFont()
         font.setBold(True)
         self.lb_npedido.setFont(font)
         self.lb_cliente = QLabel(self.centralwidget)
         self.lb_cliente.setObjectName(u"lb_cliente")
-        self.lb_cliente.setGeometry(QRect(240, 150, 47, 13))
+        self.lb_cliente.setGeometry(QRect(390, 170, 47, 13))
         self.lb_cliente.setFont(font)
         self.lb_tipovenda = QLabel(self.centralwidget)
         self.lb_tipovenda.setObjectName(u"lb_tipovenda")
-        self.lb_tipovenda.setGeometry(QRect(410, 150, 91, 16))
+        self.lb_tipovenda.setGeometry(QRect(700, 170, 91, 16))
         self.lb_tipovenda.setFont(font)
         self.lb_prazopgt = QLabel(self.centralwidget)
         self.lb_prazopgt.setObjectName(u"lb_prazopgt")
-        self.lb_prazopgt.setGeometry(QRect(80, 270, 71, 16))
+        self.lb_prazopgt.setGeometry(QRect(80, 240, 71, 16))
         self.lb_prazopgt.setFont(font)
         self.lb_vendedor = QLabel(self.centralwidget)
         self.lb_vendedor.setObjectName(u"lb_vendedor")
-        self.lb_vendedor.setGeometry(QRect(240, 270, 91, 16))
+        self.lb_vendedor.setGeometry(QRect(390, 240, 91, 16))
         self.lb_vendedor.setFont(font)
         self.lb_ordemcompra = QLabel(self.centralwidget)
         self.lb_ordemcompra.setObjectName(u"lb_ordemcompra")
-        self.lb_ordemcompra.setGeometry(QRect(410, 270, 121, 16))
+        self.lb_ordemcompra.setGeometry(QRect(700, 240, 121, 16))
         self.lb_ordemcompra.setFont(font)
         self.button_home_2 = QPushButton(self.centralwidget)
         self.button_home_2.setObjectName(u"button_home_2")
-        self.button_home_2.setGeometry(QRect(90, 400, 71, 31))
+        self.button_home_2.setGeometry(QRect(310, 370, 191, 71))
         self.button_home_2.setCursor(QCursor(Qt.PointingHandCursor))
         self.button_home_2.setStyleSheet(u"QPushButton\n"
 "{\n"
 "    background-color: rgb(93, 215, 241); \n"
 "    border: 1px solid rgb(93, 215, 241);\n"
 "    border-radius: 5px;\n"
-"    font-size: 10px;    \n"
+"    font-size: 10px;\n"
+"	color: #fff\n"
 "}\n"
 "QPushButton:hover\n"
 "{\n"
@@ -110,26 +112,49 @@ class Ui_PedVendaPage(object):
 "}")
         self.button_gnfe = QPushButton(self.centralwidget)
         self.button_gnfe.setObjectName(u"button_gnfe")
-        self.button_gnfe.setGeometry(QRect(430, 400, 71, 31))
+        self.button_gnfe.setGeometry(QRect(600, 370, 191, 71))
         self.button_gnfe.setCursor(QCursor(Qt.PointingHandCursor))
         self.button_gnfe.setStyleSheet(u"QPushButton\n"
 "{\n"
 "    background-color: rgb(93, 215, 241); \n"
 "    border: 1px solid rgb(93, 215, 241);\n"
 "    border-radius: 5px;\n"
-"    font-size: 10px;    \n"
+"    font-size: 10px;\n"
+"	color: #fff;\n"
 "}\n"
 "QPushButton:hover\n"
 "{\n"
 "    background-color: #fff;\n"
 "    color : rgb(93, 215, 241);\n"
 "}")
+        self.wrapperHeader = QGraphicsView(self.centralwidget)
+        self.wrapperHeader.setObjectName(u"wrapperHeader")
+        self.wrapperHeader.setGeometry(QRect(0, 0, 1101, 50))
+        self.wrapperHeader.setStyleSheet(u"background-color: rgb(0, 0, 0);")
+        self.exitButton = QPushButton(self.centralwidget)
+        self.exitButton.setObjectName(u"exitButton")
+        self.exitButton.setGeometry(QRect(980, 10, 105, 31))
+        self.exitButton.setCursor(QCursor(Qt.PointingHandCursor))
+        self.exitButton.setStyleSheet(u"QPushButton\n"
+"{\n"
+"	background-color: rgb(93, 215, 241); \n"
+"	border: 1px solid rgb(93, 215, 241);\n"
+"	border-radius: 5px;\n"
+"	font-size: 10px;\n"
+"	color: #fff;\n"
+"}\n"
+"QPushButton:hover\n"
+"{\n"
+"	background-color: #000000;\n"
+"	color : rgb(93, 215, 241);\n"
+"}\n"
+"")
         self.homeTitle = QLabel(self.centralwidget)
         self.homeTitle.setObjectName(u"homeTitle")
-        self.homeTitle.setGeometry(QRect(230, 10, 161, 31))
+        self.homeTitle.setGeometry(QRect(530, 20, 101, 16))
         self.homeTitle.setStyleSheet(u"background-color: transparent;\n"
 "color: #fff;\n"
-"font-size: 15px")
+"font-size: 12px")
         self.homeButton = QPushButton(self.centralwidget)
         self.homeButton.setObjectName(u"homeButton")
         self.homeButton.setGeometry(QRect(10, 10, 105, 31))
@@ -141,28 +166,8 @@ class Ui_PedVendaPage(object):
 "	background-color: rgb(93, 215, 241); \n"
 "	border: 1px solid rgb(93, 215, 241);\n"
 "	border-radius: 5px;\n"
-"	font-size: 10px;	\n"
-"}\n"
-"QPushButton:hover\n"
-"{\n"
-"	background-color: #000000;\n"
-"	color : rgb(93, 215, 241);\n"
-"}\n"
-"")
-        self.wrapperHeader = QGraphicsView(self.centralwidget)
-        self.wrapperHeader.setObjectName(u"wrapperHeader")
-        self.wrapperHeader.setGeometry(QRect(0, 0, 601, 50))
-        self.wrapperHeader.setStyleSheet(u"background-color: rgb(0, 0, 0);")
-        self.exitButton = QPushButton(self.centralwidget)
-        self.exitButton.setObjectName(u"exitButton")
-        self.exitButton.setGeometry(QRect(490, 10, 105, 31))
-        self.exitButton.setCursor(QCursor(Qt.PointingHandCursor))
-        self.exitButton.setStyleSheet(u"QPushButton\n"
-"{\n"
-"	background-color: rgb(93, 215, 241); \n"
-"	border: 1px solid rgb(93, 215, 241);\n"
-"	border-radius: 5px;\n"
-"	font-size: 10px;	\n"
+"	font-size: 10px;\n"
+"	color: #fff;\n"
 "}\n"
 "QPushButton:hover\n"
 "{\n"
@@ -171,26 +176,6 @@ class Ui_PedVendaPage(object):
 "}\n"
 "")
         PedVendaPage.setCentralWidget(self.centralwidget)
-        self.embrulho_2.raise_()
-        self.wrapperHeader.raise_()
-        self.homeButton.raise_()
-        self.homeTitle.raise_()
-        self.exitButton.raise_()
-        self.textBrowser_3.raise_()
-        self.line_npedido.raise_()
-        self.line_tipovenda.raise_()
-        self.line_ordemcompra.raise_()
-        self.line_cliente.raise_()
-        self.line_prazopgt.raise_()
-        self.line_vendedor.raise_()
-        self.lb_npedido.raise_()
-        self.lb_cliente.raise_()
-        self.lb_tipovenda.raise_()
-        self.lb_prazopgt.raise_()
-        self.lb_vendedor.raise_()
-        self.lb_ordemcompra.raise_()
-        self.button_home_2.raise_()
-        self.button_gnfe.raise_()
         QWidget.setTabOrder(self.homeButton, self.exitButton)
         QWidget.setTabOrder(self.exitButton, self.line_npedido)
         QWidget.setTabOrder(self.line_npedido, self.line_cliente)
@@ -275,9 +260,9 @@ class Ui_PedVendaPage(object):
         self.lb_ordemcompra.setText(QCoreApplication.translate("PedVendaPage", u"ORDEM DE COMPRA", None))
         self.button_home_2.setText(QCoreApplication.translate("PedVendaPage", u"GRAVAR", None))
         self.button_gnfe.setText(QCoreApplication.translate("PedVendaPage", u"GERAR NFE", None))
-        self.homeTitle.setText(QCoreApplication.translate("PedVendaPage", u"RESOLUTION SPEED", None))
-        self.homeButton.setText(QCoreApplication.translate("PedVendaPage", u"HOME", None))
         self.exitButton.setText(QCoreApplication.translate("PedVendaPage", u"SAIR", None))
+        self.homeTitle.setText(QCoreApplication.translate("PedVendaPage", u"Pedido de Venda", None))
+        self.homeButton.setText(QCoreApplication.translate("PedVendaPage", u"HOME", None))
     # retranslateUi
 
 if __name__ == "__main__":
